37038135c726ee50972727e4e992512e910d1e3b
Mathew Jacob <mathewpothanamuzhiyil@gmail.com>
2023-04-25 16:40:37 +0400

    Sessiion4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,16 +1,17 @@
- 
+
 using Microsoft.AspNetCore.Mvc;
-using Domain; 
+using Domain;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-
+using MediatR;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController:ControllerBase
+    public class BaseApiController : ControllerBase
     {
-
+        private IMediator? _mediator;
+        protected IMediator  Mediator => _mediator ??  HttpContext.RequestServices.GetService<IMediator>();
     }
 }
