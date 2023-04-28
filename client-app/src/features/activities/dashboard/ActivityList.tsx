1ffc72f9c53a6efa738a0a2deb6ca68b945b288c
Mathew Jacob <mathewpothanamuzhiyil@gmail.com>
2023-04-28 10:58:59 +0400

    Session 6.1

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -17,6 +17,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
+    
     return (
         <Segment>
             <Item.Group devided>
